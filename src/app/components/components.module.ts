876b44aa2726708be47b3e49dd2749ec8edeba01
Shees_Ali <sheessyed101@gmail.com>
2023-03-29 16:59:27 +0500

    basic structure for app and theming updates

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FormsModule } from '@angular/forms';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, FormsModule],
-  exports: [],
+  declarations: [SideNavComponent, ToolbarComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
+  exports: [SideNavComponent, ToolbarComponent],
 })
 export class ComponentsModule {}
