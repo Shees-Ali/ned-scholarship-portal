174cb44755fe907cdc56e93c889c61637bd7cd03
huzaifa-qureshi <huzaifa981981@gmail.com>
2023-04-04 03:06:55 +0500

    updates to login-signup page

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -3,9 +3,14 @@ import { CommonModule } from '@angular/common';
 import { AuthenticationPage } from './authentication.page';
 import { AuthenticationRoutingModule } from './authentication-routing.module';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule} from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatGridListModule } from '@angular/material/grid-list';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
   declarations: [AuthenticationPage],
-  imports: [CommonModule, AuthenticationRoutingModule, MatButtonModule],
+  imports: [CommonModule, AuthenticationRoutingModule, MatButtonModule, MatCardModule,
+  MatInputModule, MatGridListModule, MatFormFieldModule],
 })
 export class AuthenticationModule {}
