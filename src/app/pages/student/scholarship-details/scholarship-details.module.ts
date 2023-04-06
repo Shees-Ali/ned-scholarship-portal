38001b4437a41395db686149451b522e4f06043a
Shees_Ali <sheessyed101@gmail.com>
2023-04-06 22:35:25 +0500

    change

--- src/app/pages/student/scholarship-details/scholarship-details.module.ts
+++ src/app/pages/student/scholarship-details/scholarship-details.module.ts
@@ -1,17 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ScholarshipDetailsComponent } from './scholarship-details.page';
+import { ScholarshipDetailsPage } from './scholarship-details.page';
 import { ScholarshipDetailsRoutingModule } from './scholarship-details-routing.module';
 
-
-
 @NgModule({
-  declarations: [
-    ScholarshipDetailsComponent
-  ],
-  imports: [
-    CommonModule,
-    ScholarshipDetailsRoutingModule
-  ]
+  declarations: [ScholarshipDetailsPage],
+  imports: [CommonModule, ScholarshipDetailsRoutingModule],
 })
-export class ScholarshipDetailsModule { }
+export class ScholarshipDetailsModule {}
