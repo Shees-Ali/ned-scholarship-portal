4913ce780854739ee5ddc7c9475642f94c9ffd46
huzaifa-qureshi <huzaifa981981@gmail.com>
2023-04-17 02:21:50 +0500

    changes

--- src/app/pages/student/dashboard/dashboard.page.ts
+++ src/app/pages/student/dashboard/dashboard.page.ts
@@ -7,11 +7,22 @@ import { BasePage } from 'src/app/base/base.page';
   styleUrls: ['./dashboard.page.scss'],
 })
 export class DashboardPage extends BasePage implements OnInit {
+  scholarshipList: any[] = [];
+  limit: number = 3;
+  lastItem: any = undefined;
   constructor(injector: Injector) {
     super(injector);
   }
 
   ngOnInit() {
     this.utiltiy.isPages.next(true);
+    this.getData();
+  }
+
+  async getData() {
+    this.scholarshipList = await this.scholarshipService.getScholarShipList(
+      this.limit,
+      this.lastItem ? this.lastItem.key : undefined
+    );
   }
 }
