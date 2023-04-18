e667115822808bb332ffc54db8ad4aedbe82886d
DESKTOP-ML2KBGV\pc <a.rafayiman@gmail.com>
2023-04-18 13:04:53 -0700

    home page code

---
+++ src/app/home/about-footer/about-footer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-footer',
+  templateUrl: './about-footer.component.html',
+  styleUrls: ['./about-footer.component.scss']
+})
+export class AboutFooterComponent {
+
+}
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePage } from './home.page';
 import { HomeRoutingModule } from './home-routing.module';
+import { FaqPageComponent } from './faq-page/faq-page.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { AboutFooterComponent } from './about-footer/about-footer.component';
+import { ImageCarouselComponent } from './image-carousel/image-carousel.component';
+import { MatInputModule } from '@angular/material/input';
 
 
 
 @NgModule({
   declarations: [
-    HomePage
-  ],
+    HomePage,
+    FaqPageComponent,
+    FeedbackComponent,
+    AboutFooterComponent,
+    ImageCarouselComponent,
+   ],
   imports: [
     CommonModule,
     HomeRoutingModule
