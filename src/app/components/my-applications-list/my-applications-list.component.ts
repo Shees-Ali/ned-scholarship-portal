564066b97bde9b919fd733f51a3c0c9d2fd945ea
huzaifa-qureshi <huzaifa981981@gmail.com>
2023-04-24 22:11:41 +0500

    my-application table updated

--- src/app/components/my-applications-list/my-applications-list.component.ts
+++ src/app/components/my-applications-list/my-applications-list.component.ts
@@ -1,67 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
 
-//All of this will be replaced with Firebase once we integrate it
-export interface ApplicationList {
-  id: number;
-  type: string;
-  date: string;
-  app_status: string;
-  view: string;
-  download: string;
-}
-
-const DEPENDENTS_DATA: ApplicationList[] = [
-  {
-    id: 1231,
-    type: 'scholarship',
-    date: '10/10/10',
-    app_status: 'under-review',
-    view: 'view/edit',
-    download: 'download',
-  },
-  {
-    id: 2311,
-    type: 'scholarship',
-    date: '10/10/10',
-    app_status: 'under-review',
-    view: 'view/edit',
-    download: 'download',
-  },
-];
-
-const COLUMNS_SCHEMA = [
-  {
-    key: 'id',
-    type: 'number',
-    label: 'Application ID',
-  },
-  {
-    key: 'type',
-    type: 'text',
-    label: 'Type',
-  },
-  {
-    key: 'date',
-    type: 'text',
-    label: 'Date',
-  },
-  {
-    key: 'app_status',
-    type: 'string',
-    label: 'Application Status',
-  },
-  {
-    key: 'view',
-    type: 'text',
-    label: 'View/Edit',
-  },
-  {
-    key: 'download',
-    type: 'text',
-    label: 'Download',
-  },
-];
-
 @Component({
   selector: 'my-applications-list',
   templateUrl: './my-applications-list.component.html',
@@ -69,17 +7,6 @@ const COLUMNS_SCHEMA = [
 })
 export class MyApplicationsListComponent implements OnInit {
   @Input('application_list') application_list?: Array<any>;
-
-  displayedColumns: string[] = [
-    'id',
-    'type',
-    'date',
-    'app_status',
-    'view',
-    'download',
-  ];
-  dataSource: any = DEPENDENTS_DATA;
-  columnsSchema: any = COLUMNS_SCHEMA;
 
   ngOnInit() {
     console.log(this.application_list);
